2d30685c81a8a63df5b54f590bb9dc0f1bb177f3
15291783517 <848671907@qq.com>
2017-10-24 17:49:03 +0800

    second commit

--- app/src/main/java/com/example/logindemo/ChatActivity.java
+++ app/src/main/java/com/example/logindemo/ChatActivity.java
@@ -1,11 +1,16 @@
 package com.example.logindemo;
 
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -13,7 +18,6 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_fragment);
+
     }
-
-
 }
--- app/src/main/java/com/example/logindemo/Fragment/StudyFragment.java
+++ app/src/main/java/com/example/logindemo/Fragment/StudyFragment.java
@@ -1,9 +1,12 @@
 package com.example.logindemo.Fragment;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,8 +15,33 @@ import com.example.logindemo.R;
 public class StudyFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.activity_study_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_study_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.navigation, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.navigetion_item:
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 }
